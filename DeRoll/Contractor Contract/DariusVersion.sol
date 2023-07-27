2a070a22d415460ea23d25a11ab2a46dd20cb7b2
Darius Tanz <scratchticketseb@gmail.com>
2023-07-27 16:51:27 +0000

    Jul 27, 2023, 12:51 PM

--- DeRoll/Contractor Contract/DariusVersion.sol
+++ DeRoll/Contractor Contract/DariusVersion.sol
@@ -29,8 +29,9 @@ contract ContractorJobs{
     }
 
     function CreateJob(string memory description, string memory link, uint256 payout) public onlyOwner{
-        require(bytes(description).length > 0 && bytes(description).length < 200, "");
+        require(bytes(description).length > 0 && bytes(description).length < 200, "Description is either empty or too long.");
 
+        
         uint256 JobId = ContractorJobsCount;
         Jobs[JobId] = Job(description, link, payout, address(0), false, false);
         ContractorJobsCount++;
