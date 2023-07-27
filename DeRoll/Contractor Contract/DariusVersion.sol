20bdb7cb052219fc59ac0e36e0f82a58ef84c0df
Darius Tanz <scratchticketseb@gmail.com>
2023-07-27 17:01:23 +0000

    Jul 27, 2023, 1:01 PM

--- DeRoll/Contractor Contract/DariusVersion.sol
+++ DeRoll/Contractor Contract/DariusVersion.sol
@@ -10,7 +10,7 @@ contract ContractorJobs{
     //Other Declarations
     mapping(address => bool) public Contractors;
     mapping(uint256 => Job) public Jobs;
-    mapping(uint256 => uint256) internal 
+    mapping(uint256 => uint256) internal FisherYatesShuffle;
 
     modifier onlyOwner(){
         require(msg.sender == Owner, "Only the owner can call this function.");
