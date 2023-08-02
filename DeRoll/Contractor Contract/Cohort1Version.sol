5af7325c6408c12376b8eb81c0d548fa87c2d852
desznaj <57845143+desznaj@users.noreply.github.com>
2023-08-02 20:17:47 +0200

    Update Cohort1Version.sol

---
+++ DeRoll/Contractor Contract/Cohort1Version.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./ICohort1Version.sol";
+
+contract Cohort1Version is ICohort1Version, Ownable {
+  using Counters for Counters.Counter;
+
+  Counters.Counter private jobIdCounter;
+
+  mapping(address => bool) private Contractors;
+  mapping(uint256 => Job) private Jobs;
+
+  // Contractor Functions
+
+
+
+  // Owner Only Functions
+
+  function createJob(string memory _title, string memory _description, string memory _link, uint256 _payout) external onlyOwner {
+    jobIdCounter.increment();
+
+    Jobs[jobIdCounter.current()] = Job(
+      jobIdCounter.current(), 
+      _title, 
+      _description,
+      _link, 
+      _payout, 
+      address(0), 
+      JobStatus.AVAILABLE
+    );
+  }
+
+  function addContractors(address[] memory _contractors) external onlyOwner {
+    for (uint256 i = 0; i < _contractors.length; i++) {
+      Contractors[_contractors[i]] = true;
+    }
+    emit ContractorsAdded(_contractors);
+  }
+
+  function removeContractors(address[] memory _contractors) external onlyOwner {
+    for (uint256 i = 0; i < _contractors.length; i++) {
+      Contractors[_contractors[i]] = false;
+    }
+    emit ContractorsRemoved(_contractors);
+  }
+}
