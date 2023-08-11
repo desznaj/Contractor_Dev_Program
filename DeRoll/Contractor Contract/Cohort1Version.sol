dc11c210804566d5b68422f39b6ba1f38779aa5f
desznaj <57845143+desznaj@users.noreply.github.com>
2023-08-11 09:38:13 +0100

    Update Cohort1Version.sol

--- DeRoll/Contractor Contract/Cohort1Version.sol
+++ DeRoll/Contractor Contract/Cohort1Version.sol
@@ -6,60 +6,60 @@ import "@openzeppelin/contracts/utils/Counters.sol";
 import "./ICohort1Version.sol";
 
 contract Cohort1Version is ICohort1Version, Ownable {
-  using Counters for Counters.Counter;
+    using Counters for Counters.Counter;
 
-  Counters.Counter private jobIdCounter;
+    Counters.Counter private jobIdCounter;
 
-  mapping(address => bool) private Contractors;
-  mapping(uint256 => Job) private Jobs;
+    mapping(address => bool) private Contractors;
+    mapping(uint256 => Job) private Jobs;
 
-  // Contractor Functions
+    // Contractor Functions
 
 
 
-  // Owner Only Functions
+    // Owner Only Functions
 
-  function createJob(string memory _title, string memory _description, string memory _link, uint256 _payout) external onlyOwner {
-    jobIdCounter.increment();
-    uint256 newJobId = jobIdCounter.current();
+    function createJob(string memory _title, string memory _description, string memory _link, uint256 _payout) external onlyOwner {
+        jobIdCounter.increment();
+        uint256 newJobId = jobIdCounter.current();
 
-    Jobs[newJobId] = Job(
-      newJobId, 
-      _title, 
-      _description,
-      _link, 
-      _payout, 
-      address(0), 
-      JobStatus.AVAILABLE
-    );
+        Jobs[newJobId] = Job(
+            newJobId, 
+            _title, 
+            _description,
+            _link, 
+            _payout, 
+            address(0), 
+            JobStatus.AVAILABLE
+        );
 
-    emit JobCreated(newJobId, _title, _description, _link, _payout);
-  }
-
-  function finalizeJob(uint256 _jobId) public onlyOwner {
-    require(jobId < Jobs.length, "Job ID not valid");
-    require(!Jobs[_jobId].Status == JobStatus.ACCEPTED, "Job already completed");
-
-    Jobs[_jobId].Completed = true;
-
-    uint256 payoutAmount = jobs[_jobId].totalPayout / 2;
-    (bool success, ) = payable(msg.sender).call{value: payoutAmount}("");
-    require(success, "Failed to send funds to the contractor.");
-
-    emit JobFinalized(_jobId);
-  }
-
-  function addContractors(address[] memory _contractors) external onlyOwner {
-    for (uint256 i = 0; i < _contractors.length; i++) {
-      Contractors[_contractors[i]] = true;
+        emit JobCreated(newJobId, _title, _description, _link, _payout);
     }
-    emit ContractorsAdded(_contractors);
-  }
 
-  function removeContractors(address[] memory _contractors) external onlyOwner {
-    for (uint256 i = 0; i < _contractors.length; i++) {
-      Contractors[_contractors[i]] = false;
+    function finalizeJob(uint256 _jobId) public onlyOwner {
+        require(jobId < Jobs.length, "Job ID not valid");
+        require(!Jobs[_jobId].Status == JobStatus.ACCEPTED, "Job already completed");
+    
+        Jobs[_jobId].Completed = true;
+    
+        uint256 payoutAmount = jobs[_jobId].totalPayout / 2;
+        (bool success, ) = payable(msg.sender).call{value: payoutAmount}("");
+        require(success, "Failed to send funds to the contractor.");
+    
+        emit JobFinalized(_jobId);
     }
-    emit ContractorsRemoved(_contractors);
-  }
+
+    function addContractors(address[] memory _contractors) external onlyOwner {
+        for (uint256 i = 0; i < _contractors.length; i++) {
+          Contractors[_contractors[i]] = true;
+        }
+        emit ContractorsAdded(_contractors);
+    }
+
+    function removeContractors(address[] memory _contractors) external onlyOwner {
+        for (uint256 i = 0; i < _contractors.length; i++) {
+            Contractors[_contractors[i]] = false;
+        }
+        emit ContractorsRemoved(_contractors);
+    }
 }
